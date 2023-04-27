76a032adc558a6c8d3b56236067c66dc2e524960
SebDooley <114167816+SebDooley@users.noreply.github.com>
2023-04-27 16:40:57 +0100

    Update finite_diff_solver.f90
    Initial changes/fixes to code. Implemented changes agreed with Fraser (original coder).
    Initial inspection of produced graphs in pyplotter is promising.
    ireal change applied naively - double check required.

--- finite_diff_solver.f90
+++ finite_diff_solver.f90
@@ -1,6 +1,6 @@
 !NOTE - COMPILE THIS PROGRAM WITH THE FOLLOWING LINE
 !gfortran finite_diff_solver.f90 -llapack -o finite_diff_solver
-PROGRAM MAIN 
+PROGRAM MAIN
     USE ISO_FORTRAN_ENV
     IMPLICIT NONE
     INTEGER, PARAMETER :: n=2500 !node number
@@ -8,6 +8,7 @@ PROGRAM MAIN
     INTEGER, DIMENSION(n) :: ipiv
     INTEGER, PARAMETER :: tsteps = 100
     REAL(REAL64), DIMENSION(n,n) :: A !solver matrix
+    REAL(REAL64), DIMENSION(n,n) :: A_copy !copy of solver matrix
     REAL(REAL64), DIMENSION(n) :: c,b !c vector for solving 
     REAL(REAL64), DIMENSION(n,tsteps) :: cstorage
     REAL(REAL64) :: c0 !initial c value
@@ -20,10 +21,10 @@ PROGRAM MAIN
     REAL(REAL64), DIMENSION(tsteps) :: iapp, Z !applied current, in general a function of t
     REAL(REAL64) :: a_small, F, L !constants
     R  = 5.22_REAL64*(10.0_REAL64**(-6))
-    deltar = R/real(n,kind=REAL64)
-    dt = 0.1_REAL64 !set same as delta r for now
+    deltar = R/(REAL(n,kind=REAL64)-1.0_REAL64)
+    dt = 0.1_REAL64 !set similar to delta r for now
     D = (3.3_REAL64*(10.0_REAL64**(-13)))
-    k = -(D)/(2.0_REAL64*(deltar**2)) !k is just a shortcut holding -D/(2(dr^2))
+    k = -D/(2.0_REAL64*(deltar**2)) !k is just a shortcut holding -D/(2(dr^2))
     
     c0 = 0.0_REAL64
     c = c0 !set c to initial state
@@ -35,34 +36,50 @@ PROGRAM MAIN
     iapp(tsteps/2:tsteps) = -0.73_REAL64*(10.0_REAL64**(-3))
     Z = (-iapp)/(a_small*F*L*D)
     cstorage(:,1) = c0 !set first entry in storage vector to initial concentration
+
+    !build A matrix for solver (constant over time)
+    A = 0.0_REAL64
+    !boundary condition
+    !centre of sphere (left edge)
+    A(1,1) = 1.0_REAL64 - 2.0_REAL64*k*dt
+    A(1,2) = 2.0_REAL64*k*dt
+    !Interior
+    DO i = 2,n-1 !iterate through matrix, building each component 
+        ireal = real(i-1,kind=REAL64) !(i-1) instead of i, due to r_1 = 0.0 (indexing from 1)
+        A(i,i-1) = k*dt*(1.0_REAL64 - (1.0_REAL64/ireal))
+        A(i,i) = 1.0_REAL64 - (2.0_REAL64*k*dt)
+        A(i,i+1) = k*dt*(1.0_REAL64 + (1.0_REAL64/ireal))
+    END DO
+    !boundary condition
+    !edge of sphere (right edge)
+    A(n,n-1) = 2.0_REAL64*k*dt
+    A(n,n) = (1.0_REAL64) + ((D/(deltar**2))*dt)
+    !print*, 'a',A
+    A_copy = A
+
     DO tstep = 1,(tsteps-1) !we have the first state (j=1), and each loop finds the j+1th state, so we go to tsteps-1.
         !build solver
-        A = 0.0_REAL64
+        !boundary condition
+        !centre of sphere (left edge)
+        b(1) = (D/(deltar**2))*(c(2) - c(1))*dt + (c(1))
+
         DO i = 2,n-1 !iterate through matrix, building each component 
-            ireal = real(i,kind=REAL64)
-            A(i,i-1) = k*(1.0_REAL64 - (1.0_REAL64/ireal))
-            A(i,i) = (1.0_REAL64/dt - (2.0_REAL64)*(k))
-            A(i,i+1) = k*(1.0_REAL64 + (1.0_REAL64/ireal))
-            b(i) = (D/2.0_REAL64)*((c(i+1)-2*c(i)+c(i-1))/(deltar**2))+&
-            ((D/(ireal*deltar))*((c(i+1)-c(i-1))/(2*deltar))) + c(i)/dt
+            ireal = real(i-1,kind=REAL64)
+            b(i) = (D/2.0_REAL64)*((c(i+1)-2*c(i)+c(i-1))/(deltar**2))*dt+&
+            ((D/(ireal*deltar))*((c(i+1)-c(i-1))/(2*deltar)))*dt + c(i)
         END DO
         
-        !boundary conditions
-        !centre of sphere (left edge)
-        A(1,1) = (1.0_REAL64/dt) - 2.0_REAL64*k
-        A(1,2) = 2.0_REAL64*k
-        b(1) = (D/(deltar**2))*(c(2) - c(1)) + (c(1)/dt)
-
+        !boundary condition
         !edge of sphere (right edge)
-        A(n,n-1) = 2.0_REAL64*k
-        A(n,n) = (1.0_REAL64/dt) + (D/(deltar**2))
-        b(n) = ((D/R)*(Z(tstep+1) + Z(tstep)))+((D*Z(tstep+1))/deltar)&
-        +(D/2.0_REAL64)*(((2.0_REAL64*Z(tstep)*deltar)+(2.0_REAL64*c(N-1))-(2.0_REAL64*c(N)))/(deltar**2))
-        !print*, 'a',A
+        b(n) = ((D/R)*(Z(tstep+1) + Z(tstep)))*dt+((D*Z(tstep+1))/deltar)*dt &
+        +(D/2.0_REAL64)*dt*(((2.0_REAL64*Z(tstep)*deltar)+(2.0_REAL64*c(N-1))-(2.0_REAL64*c(N)))/(deltar**2)) + c(n)
         !print*, 'c',c
 
         !implicit scheme
-        CALL dgesv(n,1,A,n,ipiv,b,n,info)    
+        CALL dgesv(n,1,A,n,ipiv,b,n,info)
+        !print*, 'a',A
+        A = A_copy !dgesv affects matrix A
+        !print*, 'a',A
         c = b
         !IF(info/=0) THEN
         !    PRINT*,'Inversion Failed'
@@ -83,3 +100,34 @@ PROGRAM MAIN
     CLOSE(9)
 
 END PROGRAM
+
+
+!Suggested changes:
+
+!Write to output file at each time step - saves memory requirements if small timestep
+!Wouldn't need a matrix to store values.
+!Probably less efficient.
+
+!Simplify edge of sphere equation using d2c/dr2 = 0, due to substitution of equation dc/dr = Z(t) => d2c/dr2 = 0.
+!Likely won't use this change - including term allows for consideration of non-zero d2c/dr2, i.e. numerical errors.
+
+
+!Change log:
+
+!Line 23 (original):
+!deltar = R/(REAL(n,kind=REAL64)-1.0_REAL64)
+!instead of
+!deltar = R/REAL(n,kind=REAL64)
+!We have n nodes, so divide by (n-1)
+
+!Lines 39-48 (original):
+!Discretisation/Mesh is constant over simulation
+!Intialise matrix A moved outside of time loop and kept constant
+!Also re-ordered relating to memory storage
+!Notably - dgesv affects the matrix A, so a copy is required to store the values.
+!Still stops unnecessary extra computation - but now at the cost of memory.
+
+!ireal = real(i-1,kind=REAL64)
+!instead of
+!ireal = real(i,kind=REAL64)
+!Multiplied underlying equations through by dt for stability
