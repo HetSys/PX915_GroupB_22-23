fbcf457331dac887280b59b997c0806bca08ce58
Yu Lei <phrnmd@hetmathsys1.scrtp.warwick.ac.uk>
2023-05-16 13:06:35 +0100

    NC_output_parameter

--- nc_output.f90
+++ nc_output.f90
@@ -15,12 +15,13 @@ MODULE nc_output
     END IF
   END SUBROUTINE check
   
-  SUBROUTINE output_cstorage(cstorage, n, tsteps, filename)
+  SUBROUTINE output_cstorage(cstorage, n, tsteps, R, filename)
     ! Output the cstorage array to a NetCDF file
     INTEGER, INTENT(IN) :: n, tsteps
+    REAL(REAL64) :: R
     REAL(REAL64), DIMENSION(n, tsteps), INTENT(IN) :: cstorage
     CHARACTER(*), INTENT(IN) :: filename
-    INTEGER :: ncid, varid, dimids(2), status
+    INTEGER :: ncid, varid, varid_t, varid_n, varid_R, dimids(2),dimids_parameter(1), status
     
     ! Create NetCDF file
     CALL check(nf90_create(filename, NF90_CLOBBER, ncid))
@@ -28,15 +29,22 @@ MODULE nc_output
     ! Define dimensions
     CALL check(status = nf90_def_dim(ncid, "n", n, dimids(1)))
     CALL check(status = nf90_def_dim(ncid, "tsteps", tsteps, dimids(2)))
+    CALL check(status = nf90_def_dim(ncid, "parameter", 1, dimids_parameter(1)))
     
     ! Define variables
     CALL check(status = nf90_def_var(ncid, "cstorage", NF90_DOUBLE, dimids, varid))
+    CALL check(status = nf90_def_var(ncid, "tsteps", NF90_INT,dimids_parameter,varid_t))
+    CALL check(status = nf90_def_var(ncid, "node_num", NF90_INT,dimids_parameter,varid_n))
+    CALL check(status = nf90_def_var(ncid, "R", NF90_DOUBLE,dimids_parameter,varid_R))
     
     ! End define mode and switch to data mode
     CALL check(status = nf90_enddef(ncid))
-    
+
     ! Write data to variable
     CALL check(status = nf90_put_var(ncid, varid, cstorage))
+    CALL check(status = nf90_put_var(ncid, varid_t, tsteps))
+    CALL check(status = nf90_put_var(ncid, varid_n, n))
+    CALL check(status = nf90_put_var(ncid, varid_R, R))
     
     ! Close the file
     CALL check(status = nf90_close(ncid))
