a7787a67be750fe6b36d7e3eb156ceda112660f1
Fraser-Birks <fraser.birks@warwick.ac.uk>
2023-05-24 12:48:10 +0100

    Added comments and unit testing and lithium mass loss calculation

---
+++ convergence_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import unit_test
+import matplotlib.pyplot as plt
+
+#run convergence testing on dt
+
+dt_vals = [0.01,0.05,0.1,0.5,1.0,5.0,10.0]
+final_voltage = np.array([])
+final_mass_loss = np.array([])
+for dt in dt_vals:
+    v, mass_loss = unit_test.run_unit_test(dt=dt,nodenum=200)
+    final_voltage.append(v)
+    final_mass_loss.append(mass_loss)
+
+plt.plot(np.log10(dt_vals),np.log10(final_mass_loss))
+plt.xlabel('log10(dt(s))')
+plt.ylabel('log10(Lithium Mass Loss (g))')
+plt.title('Convergence of lithium mass loss against timestep')
+plt.savefig('dt_vs_mass_loss.png')
+
+plt.figure()
+plt.plot(np.log10(dt_vals),np.log10(np.abs(final_voltage-(final_voltage[0]))))
+plt.xlabel('log10(dt(s))')
+plt.ylabel('log10(Error in final cell voltage (V))')
+plt.title('Convergence of error in final cell voltage against timestep size')
+plt.savefig('dt_vs_voltage.png')
+#measure the convergence of voltage and lithium mass loss with number of nodes
+
